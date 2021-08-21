8675ba258a06038f3454b878ed0575479584ddcf
Ramil Gilyazev <r.a.gilyazev@gmail.com>
2021-08-21 14:09:37 +0300

    Adding simple background

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,11 @@
 import { Module } from '../core/module';
-
+import { random } from "../utils"
 export class BackgroundModule extends Module {
 
+changeBackgroundColor(){
+    document.body.style.backgroundColor=
+    'rgb(' + `${random(0,255)}`+
+    ',' +  `${random(0,255)}`+
+    ',' + `${random(0,255)}`+')';
+  }
 }
