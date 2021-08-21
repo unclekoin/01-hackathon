49a91c2f293eb829ed9de6d7749447984e56ffdb
Ramil Gilyazev <r.a.gilyazev@gmail.com>
2021-08-21 15:41:34 +0300

    Change trigger method

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -2,14 +2,16 @@ import { Module } from '../core/module';
 import { random } from "../utils"
 export class BackgroundModule extends Module {
 
+  changeBackgroundColor(){
+    document.body.style.backgroundColor=
+    `hsl(
+      ${random(0,360)},
+      ${random(0,100)}%,
+      ${random(0,100)}%)
+    `
+  }
+
   trigger(){
-    function changeBackgroundColor(){
-      document.body.style.backgroundColor=
-      `hsl(
-        ${random(0,360)},
-        ${random(0,100)}%,
-        ${random(0,100)}%)
-      `
-    }
+    this.changeBackgroundColor();
   }
 }
