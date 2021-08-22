649a58415066eb1daf11f6ab832f810062f1ea6b
Valeriy Harlamov <valera545@gmail.com>
2021-08-22 11:06:40 +0300

    Background lighted

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -5,9 +5,9 @@ export class BackgroundModule extends Module {
   changeBackgroundColor(){
     document.body.style.backgroundColor=
       `hsl(
-      ${random(0,360)},
-      ${random(30,80)}%,
-      ${random(70,100)}%)
+      ${random(0, 360)},
+      ${random(30, 80)}%,
+      ${random(70, 90)}%)
     `
   }
 
